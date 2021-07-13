181544ea430a292f4d267140193ae27f3a06bd32
Luiz Eduardo <luizeduardosad@hotmail.com>
2021-07-13 12:26:39 -0300

    Funcionalidade de busca implementada e funcional

--- js/index.js
+++ js/index.js
@@ -1,21 +1,55 @@
 (function () {
 
   // VARIÁVEIS
-  let divBoxes = Array.prototype.slice.call((document.querySelectorAll('label[for="slide"] div')));
+  let divBoxes = Array.prototype.slice.call((document.querySelectorAll('.slide .box')));
   let closeButtom = document.querySelector('.close');
+  let searchInput = document.querySelector('.search input');
+  let titleBox = Array.prototype.slice.call(document.querySelectorAll('.box p'));
+
 
   // CONTEÚDO
   // Abre janela
   divBoxes.forEach((e) => {
     e.addEventListener('click', openAction);
   });
-
   // Fecha janela janela
   closeButtom.addEventListener('click', closeAction);
 
-  // Funções
-  function openAction(){
-    if (!Array.prototype.slice.call(this.classList).indexOf(`box`)){
+  // BUSCA
+  searchInput.addEventListener('keyup', filterBoxes);
+
+
+
+
+  // FUNÇÕES
+  function filterBoxes() {
+    let valueBox = this.value.toLowerCase();
+
+    titleBox.forEach(box => {
+      let arrayBox = (box.innerText.toLowerCase());
+
+      if (!arrayBox.indexOf(valueBox)) {
+        let boxElement = box.parentElement.style;
+        boxElement.display = 'block';
+        boxElement.height = "20rem";
+        boxElement.width = "20rem";
+
+        // Remove setas
+        Array.from(document.querySelectorAll('.icons')).forEac(arrow => {
+          arrow.style.display = "none";
+        });
+      }
+
+      else {
+        box.parentElement.style.display = 'none';
+      }
+
+    });
+  }
+
+
+  function openAction() {
+    if (!Array.prototype.slice.call(this.classList).indexOf(`box`)) {
       this.classList.add('box-on');
     }
     closeButtom.classList.add('close-on');
