ecb62a909cbf33708b5d19d46f7c49c3b6028082
Luiz Eduardo <luizeduardosad@hotmail.com>
2021-07-13 16:46:51 -0300

    Select option funcionando...

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
   // VARIÁVEIS
   const divBoxes = Array.prototype.slice.call((document.querySelectorAll('.slide .box')));
-  const closeButtom = document.querySelector('.close');
+  const closeButton = document.querySelector('.close');
   const searchInput = document.querySelector('.search input');
   const titleBox = Array.prototype.slice.call(document.querySelectorAll('.box p'));
   const contentTitle = document.querySelectorAll('.content h1');
@@ -14,7 +14,7 @@
     e.addEventListener('click', openAction);
   });
   // Fecha janela janela
-  closeButtom.addEventListener('click', closeAction);
+  closeButton.addEventListener('click', closeAction);
 
   // BUSCA
   searchInput.addEventListener('keyup', filterBoxes);
@@ -25,10 +25,33 @@
     contentTitle[ index ].innerText = title.innerText;
   });
 
+  // CONTROLE
+  let selectInput = document.querySelector('#select-content');
 
+  titleBox.forEach( (item, index) => {
+    let optionSelect = document.createElement('option');
+    optionSelect.innerText = item.innerText;
+    optionSelect.id = 'opt-' + index;
+
+    selectInput.append(optionSelect);
+  });
+
+  selectInput.addEventListener('change', selectOption);
 
 
   // FUNÇÕES
+  function selectOption(){
+    let arrayOptions = Array.prototype.slice.call(selectInput);
+    arrayOptions.forEach( item => {
+      if (item.value == selectInput.value){
+        boxIdentifier = '.box-' + Number(item.id.split('-')[1]);
+        document.querySelector(boxIdentifier).classList.add('box-on');
+        closeButton.classList.add('close-on');
+      }
+    });
+  }
+
+
   function filterBoxes() {
     let valueBox = this.value.toLowerCase();
 
@@ -53,13 +76,13 @@
     if (!Array.prototype.slice.call(this.classList).indexOf(`box`)) {
       this.classList.add('box-on');
     }
-    closeButtom.classList.add('close-on');
+    closeButton.classList.add('close-on');
 
   }
   function closeAction() {
     let openWindow = document.querySelector('.box.box-on');
     openWindow.classList.remove('box-on');
-    closeButtom.classList.remove('close-on');
+    closeButton.classList.remove('close-on');
   }
 })();
 
