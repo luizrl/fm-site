a52b8c57a00411dac6592190164b10232e516e7a
Luiz Eduardo <luizeduardosad@hotmail.com>
2021-07-13 19:15:24 -0300

    Controles funcionando

--- js/index.js
+++ js/index.js
@@ -27,6 +27,8 @@
 
   // CONTROLE
   let selectInput = document.querySelector('#select-content');
+  let buttonLeft = document.querySelector('.button-left');
+  let buttonRight = document.querySelector('.button-right');
 
   titleBox.forEach( (item, index) => {
     let optionSelect = document.createElement('option');
@@ -38,14 +40,56 @@
 
   selectInput.addEventListener('change', selectOption);
 
-
+  buttonLeft.addEventListener('click', previousTab);
+  buttonRight.addEventListener('click', nextTab);
+  
+  
   // FUNÇÕES
+  function previousTab(){
+    let options = Array.prototype.slice.call(selectInput);
+    let position;
+    
+    options.forEach( (item, index) => {
+      if (item.value == selectInput.value){
+        position = index;
+      }
+    });
+
+    if (position > 0){
+      position -= 1;
+      selectInput.value = options[ position ].value;
+      selectInput.dispatchEvent(new Event('change'));
+    }
+  }
+  function nextTab(){
+    let options = Array.prototype.slice.call(selectInput);
+    let optionsLenght = options.length;
+    let position;
+    
+    options.forEach( (item, index) => {
+      if (item.value == selectInput.value){
+        position = index;
+      }
+    });
+
+    if (position >= 0 && position < (optionsLenght - 1)){
+      position += 1;
+      selectInput.value = options[ position ].value;
+      selectInput.dispatchEvent(new Event('change'));
+    }
+  }
+
   function selectOption(){
     let arrayOptions = Array.prototype.slice.call(selectInput);
+
     arrayOptions.forEach( item => {
-      if (item.value == selectInput.value){
+
+      if (item.value == selectInput.value){  
         boxIdentifier = '.box-' + Number(item.id.split('-')[1]);
-        document.querySelector(boxIdentifier).classList.add('box-on');
+
+        let box =  document.querySelector(boxIdentifier);
+        box.classList.add('box-on');
+        
         closeButton.classList.add('close-on');
       }
     });
