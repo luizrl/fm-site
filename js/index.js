63b00ce02a42513574e7ba626cf1848ce895c640
Luiz Eduardo <luizeduardosad@hotmail.com>
2021-07-13 10:28:43 -0300

    Consertando bugs

--- js/index.js
+++ js/index.js
@@ -3,30 +3,28 @@
   // VARIÁVEIS
   let divBoxes = Array.prototype.slice.call((document.querySelectorAll('label[for="slide"] div')));
   let closeButtom = document.querySelector('.close');
-  let contents = document.querySelector('.content');
-  let contentsChildren = Array.prototype.slice.call(contents.children);
 
   // CONTEÚDO
   // Abre janela
   divBoxes.forEach((e) => {
-    e.addEventListener('click', function () {
-      e.classList.add('box-on');
-      e.classList.remove('box-off');
-      closeButtom.classList.add('close-on');
-      console.log(e);
-    });
+    e.addEventListener('click', openAction);
   });
 
-  // Fechar janela
+  // Fecha janela janela
   closeButtom.addEventListener('click', closeAction);
 
-  function closeAction() {
-    let openWindow = document.querySelector('.box-on');
-    openWindow.classList.add('box-off');
-    openWindow.classList.remove('box-on');
+  // Funções
+  function openAction(){
+    if (!Array.prototype.slice.call(this.classList).indexOf(`box`)){
+      this.classList.add('box-on');
+    }
+    closeButtom.classList.add('close-on');
 
+  }
+  function closeAction() {
+    let openWindow = document.querySelector('.box.box-on');
+    openWindow.classList.remove('box-on');
     closeButtom.classList.remove('close-on');
   }
-
 })();
 
